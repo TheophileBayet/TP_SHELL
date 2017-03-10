16a0d5c6992b5db622e203d48d8d28be986749cc
Theophile Bayet <theophile.bayet@ensimag.grenoble-inp.fr>
2017-03-10 17:16:31 +0100

    first test

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /user/0/.base/bayett/home/workspace/SEPC/TP_SHELL
+# Build directory: /user/0/.base/bayett/home/workspace/SEPC/TP_SHELL
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(UnitShellTests "../tests/allShellTests.rb")
